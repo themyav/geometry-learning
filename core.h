aa91ca780eea81b385de59c98a39461020420eaf
Chechevatov <chechevatov.r@gmail.com>
2020-03-13 15:34:39 +0700

    Trying to learn how to commit

--- core.h
+++ core.h
@@ -5,4 +5,17 @@
 #ifndef PROJECT_LOOCH_CORE_H
 #define PROJECT_LOOCH_CORE_H
 
+struct Point {
+    double x, y, z;
+    
+    Point() = default;
+    
+    Point(double x_, double y_, double z_) {
+        x = x_;
+        y = y_;
+        z = z_;
+    }
+};
+
+
 #endif //PROJECT_LOOCH_CORE_H
---
+++ scenery.h
@@ -0,0 +1,8 @@
+//
+// Created by Chechevatov on 13.03.2020.
+//
+
+#ifndef PROJECT_LOOCH_SCENERY_H
+#define PROJECT_LOOCH_SCENERY_H
+
+#endif //PROJECT_LOOCH_SCENERY_H
